19fabbd8bc4b9d0d63b4b3f675129c622a27ae3e
Sahil Gupta <emailsahilgupta12@gmail.com>
2015-05-12 16:34:37 -0400

    Initial Commit

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Sahil Gupta on 2015-05-12.
+//  Copyright (c) 2015 Sahil Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorhythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorhythm
+//
+//  Created by Sahil Gupta on 2015-05-12.
+//  Copyright (c) 2015 Sahil Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
